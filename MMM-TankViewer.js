5a1123b5c5cf1d99b4b17d2b9a651cf32b6a89d7
yen <evgeny.sidorenko@gmail.com>
2021-05-29 14:25:46 +0300

    1

--- MMM-TankViewer.js
+++ MMM-TankViewer.js
@@ -9,15 +9,8 @@
 
 Module.register("MMM-TankViewer", {
 	defaults: {
-		//updateInterval: 60000,
-		//retryDelay: 5000,
-
-		host: '10.0.3.3',
-		port: 8080,
-		path: '',
-		interval: 10000,
-		message: '',
-		debug: false
+		updateInterval: 60000,
+		retryDelay: 5000
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
@@ -30,23 +23,15 @@ Module.register("MMM-TankViewer", {
 		//Flag for check if module is loaded
 		this.loaded = false;
 
-		console.log("*** Sending Socket Notification to node_helper");
-
-		self.sendSocketNotification("WS_CONNECT", { "config": self.config });
-
-		//this.updateDom();
-
-		setInterval(function() {
-			console.log("*** Send sendSocketNotification from setInterval function");
-
-			self.sendSocketNotification("MESSAGE", { "config": self.config });
-		}, this.config.interval);
+		Log.info('info');
+		Log.log('log');
+		Log.error('error');
 
 		// Schedule update timer.
-		//this.getData();
+		this.getData();
 		setInterval(function() {
 			self.updateDom();
-		}, this.config.interval);
+		}, this.config.updateInterval);
 	},
 
 	/*
@@ -55,7 +40,6 @@ Module.register("MMM-TankViewer", {
 	 * get a URL request
 	 *
 	 */
-
 	getData: function() {
 		var self = this;
 
@@ -65,17 +49,17 @@ Module.register("MMM-TankViewer", {
 		var dataRequest = new XMLHttpRequest();
 		dataRequest.open("GET", urlApi, true);
 		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
+			console.log("***> readyState: " + this.readyState);
 			if (this.readyState === 4) {
-				console.log(this.status);
+				console.log("***> status: " + this.status);
 				if (this.status === 200) {
 					self.processData(JSON.parse(this.response));
 				} else if (this.status === 401) {
 					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
+					Log.error("***> ERROR: " + self.name, this.status);
 					retry = false;
 				} else {
-					Log.error(self.name, "Could not load data.");
+					Log.error("***> " + self.name, "Could not load data.");
 				}
 				if (retry) {
 					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
@@ -86,66 +70,51 @@ Module.register("MMM-TankViewer", {
 	},
 
 
-
 	/* scheduleUpdate()
 	 * Schedule next update.
 	 *
 	 * argument delay number - Milliseconds before next update.
 	 *  If empty, this.config.updateInterval is used.
 	 */
-
 	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.interval;
+		var nextLoad = this.config.updateInterval;
 		if (typeof delay !== "undefined" && delay >= 0) {
 			nextLoad = delay;
 		}
-		nextLoad = nextLoad;
+		nextLoad = nextLoad ;
 		var self = this;
 		setTimeout(function() {
 			self.getData();
 		}, nextLoad);
 	},
 
-
 	getDom: function() {
 		var self = this;
 
+		// create element wrapper for show into the module
 		var wrapper = document.createElement("div");
-
+		// If this.dataRequest is not empty
 		if (this.dataRequest) {
-			// CanalizationTank
-			var wrapperCanalizationTank = document.createElement("div");
+			var wrapperDataRequest = document.createElement("div");
+			// check format https://jsonplaceholder.typicode.com/posts/1
+			wrapperDataRequest.innerHTML = this.dataRequest.title;
 
-			var labelCanalizationTank = document.createElement("label");
-			labelCanalizationTank.className = "label";
-			labelCanalizationTank.innerHTML = "Канализационный коллектор: ";
+			var labelDataRequest = document.createElement("label");
+			labelDataRequest.innerHTML = "TITLE";
 
-			var labelCanalizationTankValue = document.createElement("label");
-			labelCanalizationTankValue.className = "value";
-			labelCanalizationTankValue.innerHTML = "2.06 m";
 
-			wrapperCanalizationTank.appendChild(labelCanalizationTank);
-			wrapperCanalizationTank.appendChild(labelCanalizationTankValue);
-
-			// DrainageTank
-			var wrapperDrainageTank = document.createElement("div");
-
-			var labelDrainageTank = document.createElement("label");
-			labelDrainageTank.className = "label";
-			labelDrainageTank.innerHTML = "Дренажный коллектор: ";
-
-			var labelDrainageTankValue = document.createElement("label");
-			labelDrainageTankValue.className = "value";
-			//labelDrainageTankValue.innerHTML = this.dataRequest.title + " m";
-			labelDrainageTankValue.innerHTML = this.dataRequest ? JSON.stringify(this.dataRequest) : "0";
-
-			wrapperDrainageTank.appendChild(labelDrainageTank);
-			wrapperDrainageTank.appendChild(labelDrainageTankValue);
-
-			wrapper.appendChild(wrapperCanalizationTank);
-			wrapper.appendChild(wrapperDrainageTank);
+			wrapper.appendChild(labelDataRequest);
+			wrapper.appendChild(wrapperDataRequest);
 		}
 
+		// Data from helper
+		if (this.dataNotification) {
+			var wrapperDataNotification = document.createElement("div");
+			// datanotification
+			wrapperDataNotification.innerHTML =  "UPDATE" + ": " + this.dataNotification.date;
+
+			wrapper.appendChild(wrapperDataNotification);
+		}
 		return wrapper;
 	},
 
@@ -171,14 +140,11 @@ Module.register("MMM-TankViewer", {
 	},
 
 	// socketNotificationReceived from helper
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
 
-		console.log("****** socketNotificationReceived in Main Module ****** ---> " + payload);
+		console.log("***> socketNotificationReceived. Notification: " + notification + ", Payload: " + payload);
 
-		if (notification === "MMM-TankViewer-REQUEST_VALUE") {
-
-			console.log("---> Receved notification MMM-TankViewer-REQUEST_VALUE: " + payload);
-
+		if(notification === "MMM-TankViewer-NOTIFICATION_TEST") {
 			// set dataNotification
 			this.dataNotification = payload;
 			this.updateDom();
--- node_helper.js
+++ node_helper.js
@@ -1,12 +1,11 @@
 /* Magic Mirror
- * Node Helper: MMM-TankViewer
+ * Node Helper: MMM-TankViewer2
  *
  * By yen
  * MIT Licensed.
  */
 
 var NodeHelper = require("node_helper");
-const WebSocket = require("ws");
 
 module.exports = NodeHelper.create({
 
@@ -19,135 +18,16 @@ module.exports = NodeHelper.create({
 	 * argument payload mixed - The payload of the notification.
 	 */
 	socketNotificationReceived: function(notification, payload) {
-		var self = this;
-
-		console.log("---> Working notification system. Notification: ", notification, "payload: ", payload);
-
-		if (notification === "WS_CONNECT") {
-			// Connect event will be handeled internally
-
-			console.log("---> Notification WS_CONNECT");
-
-			self.config = payload.config;
-			self.connect(payload.config);
-
-			return;
-		} else if (notification === "WS_DISCONNECT") {
-			// Disconnect event will be handeled internally
-
-			console.log("---> Notification WS_DISCONNECT");
-
-			self.config = undefined;
-			self.disconnect();
-
-			return;
-		}
-
-		console.log("---> SET TIMEOUT END SEND SOCKET NOTIFICATION");
-
-		this.sendSocketNotification('MSG', {message: 'test'});
-
-		if(self.ws && self.ws.readyState === WebSocket.OPEN) {
-			self.ws.send(self.config.message, function ack(error){
-				if(error) {
-					self.error("Error while sending message: ", self.config.message);
-				}
-			});
-		} else {
-			self.debug("Can not send notification because WebSocket is not yet connected!", notification)
-		}
-
-	},
-
-	connect: function(config, callback) {
-		var self = this;
-
-		// Disconnect to assure only one instance is running.
-		self.disconnect();
-
-		const url = "ws://" + config.host + ":" + config.port + config.path;
-		self.ws = new WebSocket(url);
-
-		// Register error listener
-		self.ws.onerror = function(event){
-			if(callback) {
-				callback(event.code)
-			}
-		};
-
-		// Register open listener
-		self.ws.onopen = function open() {
-			self.debug("Connection established:", url);
-
-			// Register on close listener
-			self.ws.onclose = function close(event) {
-				self.error("Connection was closed!", event.code, event.reason);
-				self.reconnect(config);
-			};
-
-			// Register message handler
-			self.ws.onmessage = function message(event) {
-				try {
-					self.sendMessage(JSON.parse(event.data));
-				} catch(error) {
-					self.error("Error while handling event:", event, error);
-				}
-			};
-
-			// Notify callback if needed
-			if(callback) {
-				callback();
-			}
-		};
-	},
-
-	sendMessage: function(event) {
-		var self = this;
-		//console.log("Send event: ", event);
-
-		console.log("---> Sending Socket Notification to Main module: " + JSON.stringify(event));
-
-		self.sendSocketNotification("MMM-TankViewer-REQUEST_VALUE", event);
-	},
-
-	reconnect: function(config) {
-		var self = this;
-		self.debug("Trying to reconnect...");
-		self.connect(config, function(error) {
-			if(error) {
-				self.error("Error while reconnecting to websocket...", error);
-				setTimeout(function() { self.reconnect(config) }, config.interval);
-			}
-		});
-	},
-
-	disconnect: function() {
-		var self = this;
-		if (self.ws) {
-			// Unregister listener
-			self.ws.onclose = undefined;
-			self.ws.onerror = undefined;
-			self.ws.onopen = undefined;
-			self.ws.onmessage = undefined;
-
-			if(self.ws.readyState === WebSocket.OPEN) {
-				self.ws.close();
-				self.ws.terminate();
-			}
-			self.ws = undefined;
+		if (notification === "MMM-TankViewer-NOTIFICATION_TEST") {
+			console.log("---> Working notification system. Notification:", notification, "payload: ", payload);
+			// Send notification
+			this.sendNotificationTest("Hello from node_helper");
 		}
 	},
 
-	debug: function() {
-		var self = this;
-		if(config.debug) {
-			console.log(self, arguments);
-		}
-	},
-
-	error: function() {
-		var self = this;
-		console.error(self, arguments);
+	// Example function send notification test
+	sendNotificationTest: function(payload) {
+		this.sendSocketNotification("MMM-TankViewer-NOTIFICATION_TEST", payload);
 	},
 
 });
