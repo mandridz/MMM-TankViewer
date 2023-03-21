a6b0bd6736bb67497a9813e59c5720a3c5f3505a
Evgeny <evgeny.sidorenko@gmail.com>
2023-03-21 09:53:57 +0300

    add images

--- MMM-TankViewer.js
+++ MMM-TankViewer.js
@@ -86,15 +86,40 @@ Module.register("MMM-TankViewer", {
       var tdStatus = document.createElement("td");
       tdStatus.innerHTML = "Статус";
       var tdStatus1 = document.createElement("td");
-      tdStatus1.innerHTML = this.dataNotification[0].tankStatus;
+      var imgPower1 = document.createElement("img");
+      imgPower1.src =
+        this.dataNotification[0].tankStatus === "ON"
+          ? "img/power-on.png"
+          : "img/power-off.png";
+      tdStatus1.appendChild(imgPower1);
       var tdStatus2 = document.createElement("td");
-      tdStatus2.innerHTML = this.dataNotification[1].tankStatus;
+      var imgPower2 = document.createElement("img");
+      imgPower2.src =
+        this.dataNotification[1].tankStatus === "ON"
+          ? "img/power-on.png"
+          : "img/power-off.png";
+      tdStatus2.appendChild(imgPower2);
       var tdStatus3 = document.createElement("td");
-      tdStatus3.innerHTML = this.dataNotification[2].tankStatus;
+      var imgPower3 = document.createElement("img");
+      imgPower3.src =
+        this.dataNotification[2].tankStatus === "ON"
+          ? "img/power-on.png"
+          : "img/power-off.png";
+      tdStatus3.appendChild(imgPower3);
       var tdStatus4 = document.createElement("td");
-      tdStatus4.innerHTML = this.dataNotification[3].tankStatus;
+      var imgPower4 = document.createElement("img");
+      imgPower4.src =
+        this.dataNotification[3].tankStatus === "ON"
+          ? "img/power-on.png"
+          : "img/power-off.png";
+      tdStatus4.appendChild(imgPower4);
       var tdStatus5 = document.createElement("td");
-      tdStatus5.innerHTML = this.dataNotification[4].tankStatus;
+      var imgPower5 = document.createElement("img");
+      imgPower5.src =
+        this.dataNotification[4].tankStatus === "ON"
+          ? "img/power-on.png"
+          : "img/power-off.png";
+      tdStatus5.appendChild(imgPower5);
 
       var trCurrent = document.createElement("tr");
       var tdCurrent = document.createElement("td");
