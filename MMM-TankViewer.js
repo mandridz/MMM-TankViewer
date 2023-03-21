0a8944d32bd2cb8b37f1ea0546513148e2773a09
Evgeny <evgeny.sidorenko@gmail.com>
2023-03-21 08:49:15 +0300

    add tab status

--- MMM-TankViewer.js
+++ MMM-TankViewer.js
@@ -8,87 +8,120 @@
  */
 
 Module.register("MMM-TankViewer", {
-	defaults: {
-		updateInterval: 10000,
-		reconnectInterval: 5000,
-		debug: false
-	},
+  defaults: {
+    updateInterval: 10000,
+    reconnectInterval: 5000,
+    debug: false,
+  },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		var dataNotification = null;
+  start: function () {
+    var self = this;
+    var dataNotification = null;
 
-		self.sendSocketNotification("MMM-TankViewer-WS_CONNECT", { "config": self.config });
+    self.sendSocketNotification("MMM-TankViewer-WS_CONNECT", {
+      config: self.config,
+    });
 
-		// Schedule update timer.
-		setInterval(function() {
-			self.sendSocketNotification("MMM-TankViewer-WS_SEND_MESSAGE");
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
+    // Schedule update timer.
+    setInterval(function () {
+      self.sendSocketNotification("MMM-TankViewer-WS_SEND_MESSAGE");
+      self.updateDom();
+    }, this.config.updateInterval);
+  },
 
-	getDom: function() {
-		var self = this;
+  getDom: function () {
+    var self = this;
 
-		var wrapper = document.createElement("div");
+    var wrapper = document.createElement("div");
 
-		if (this.dataNotification) {
-			// CanalizationTank
-			var wrapperCanalizationTank = document.createElement("div");
+    if (this.dataNotification) {
+      // CanalizationTank
+      var wrapperCanalizationTank = document.createElement("div");
 
-			var labelCanalizationTank = document.createElement("label");
-			labelCanalizationTank.className = "label";
-			labelCanalizationTank.innerHTML = "Канализационный коллектор: ";
+      var labelCanalizationTank = document.createElement("label");
+      labelCanalizationTank.className = "label";
+      labelCanalizationTank.innerHTML = "Канализационный коллектор: ";
 
-			var labelCanalizationTankValue = document.createElement("label");
-			labelCanalizationTankValue.className = "value";
-			labelCanalizationTankValue.innerHTML = this.dataNotification[2].sonarValue + " m";
+      var labelCanalizationTankValue = document.createElement("label");
+      labelCanalizationTankValue.className = "value";
+      labelCanalizationTankValue.innerHTML =
+        this.dataNotification[2].sonarValue + " m";
 
-			wrapperCanalizationTank.appendChild(labelCanalizationTank);
-			wrapperCanalizationTank.appendChild(labelCanalizationTankValue);
+      wrapperCanalizationTank.appendChild(labelCanalizationTank);
+      wrapperCanalizationTank.appendChild(labelCanalizationTankValue);
 
-			// DrainageTank
-			var wrapperDrainageTank = document.createElement("div");
+      // DrainageTank
+      var wrapperDrainageTank = document.createElement("div");
 
-			var labelDrainageTank = document.createElement("label");
-			labelDrainageTank.className = "label";
-			labelDrainageTank.innerHTML = "Дренажный коллектор: ";
+      var labelDrainageTank = document.createElement("label");
+      labelDrainageTank.className = "label";
+      labelDrainageTank.innerHTML = "Дренажный коллектор: ";
 
-			var labelDrainageTankValue = document.createElement("label");
-			labelDrainageTankValue.className = "value";
-			labelDrainageTankValue.innerHTML = this.dataNotification[4].sonarValue + " m";
+      var labelDrainageTankValue = document.createElement("label");
+      labelDrainageTankValue.className = "value";
+      labelDrainageTankValue.innerHTML =
+        this.dataNotification[4].sonarValue + " m";
 
-			wrapperDrainageTank.appendChild(labelDrainageTank);
-			wrapperDrainageTank.appendChild(labelDrainageTankValue);
+      var tabStatus = document.createElement("table");
+      var trHeader = document.createElement("tr");
 
-			wrapper.appendChild(wrapperCanalizationTank);
-			wrapper.appendChild(wrapperDrainageTank);
-		}
+      var tdName = document.createElement("td");
+      tdName.innerHTML = "Насос";
+      var tdName1 = document.createElement("td");
+      tdName1.innerHTML = "1";
+      var tdName2 = document.createElement("td");
+      tdName2.innerHTML = "2";
+      var tdName3 = document.createElement("td");
+      tdName3.innerHTML = "3";
+      var tdName4 = document.createElement("td");
+      tdName4.innerHTML = "4";
+      var tdName5 = document.createElement("td");
+      tdName5.innerHTML = "5";
 
-		return wrapper;
-	},
+      trHeader.appendChild(tdName);
+      trHeader.appendChild(tdName1);
+      trHeader.appendChild(tdName2);
+      trHeader.appendChild(tdName3);
+      trHeader.appendChild(tdName4);
+      trHeader.appendChild(tdName5);
 
-	getScripts: function() {
-		return [];
-	},
+      tabStatus.appendChild(trHeader);
 
-	getStyles: function () {
-		return [
-			"MMM-TankViewer.css",
-		];
-	},
+      wrapperDrainageTank.appendChild(labelDrainageTank);
+      wrapperDrainageTank.appendChild(labelDrainageTankValue);
 
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		Log.info("***> socketNotificationReceived. Notification: " + notification + ", Payload: " + JSON.stringify(payload));
+      wrapper.appendChild(wrapperCanalizationTank);
+      wrapper.appendChild(wrapperDrainageTank);
 
+      wrapper.appendChild(tabStatus);
+    }
 
-		if(notification === "MMM-TankViewer-WS_RESPONSE") {
-			// set dataNotification
-			this.dataNotification = payload;
-			this.updateDom();
-		}
-	},
+    return wrapper;
+  },
+
+  getScripts: function () {
+    return [];
+  },
+
+  getStyles: function () {
+    return ["MMM-TankViewer.css"];
+  },
+
+  // socketNotificationReceived from helper
+  socketNotificationReceived: function (notification, payload) {
+    Log.info(
+      "***> socketNotificationReceived. Notification: " +
+        notification +
+        ", Payload: " +
+        JSON.stringify(payload)
+    );
+
+    if (notification === "MMM-TankViewer-WS_RESPONSE") {
+      // set dataNotification
+      this.dataNotification = payload;
+      this.updateDom();
+    }
+  },
 });
